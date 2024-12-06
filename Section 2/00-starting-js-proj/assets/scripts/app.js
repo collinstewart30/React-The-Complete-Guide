e05e9d0a058f40011308241a9b5151032209a8e5
collinstewart30 <collin.stewart30@yahoo.com>
2024-12-06 15:54:46 -0500

    notes and js practice - section 2-17

---
+++ Section 2/00-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,12 @@
+/* Section 2-16 */
+//import { apiKey } from "./util.js";
+import * as util from "./util.js";
+import apiKeydefault from "./util.js"; 
+
+console.log(util.apiKey);
+console.log(util.apiKey2);
+console.log(apiKeydefault);
+
+/* Section 2-17 */
+let userMessage = "User Message!"
+console.log(userMessage);
---
+++ Section 2/00-starting-js-proj/assets/scripts/util.js
@@ -0,0 +1,5 @@
+/* Section 2-16 */
+//only 1 default allowed
+export default "DEFAULT";
+export let apiKey = 'APIKEY';
+export let apiKey2 = 'APIKEY2';
